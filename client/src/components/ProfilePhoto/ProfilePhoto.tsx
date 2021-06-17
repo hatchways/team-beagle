b2336b31788ad4469058c90feff2df01c08ed132
Wilson Fong <wilsonfong1002@outlook.com>
2021-06-17 12:46:33 -0400

    complete first draft of profile photo settings

--- client/src/components/ProfilePhoto/ProfilePhoto.tsx
+++ client/src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -1,9 +1,38 @@
-import { Grid } from '@material-ui/core';
+import { useState } from 'react';
+import { Grid, Typography, Avatar, Button, Icon, InputBase } from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import useStyles from './useStyles';
+
+export function InputButton(): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <Button className={classes.uploadButton} variant="outlined" color="secondary" component="label">
+      Upload a file from your device
+      <input type="file" hidden />
+    </Button>
+  );
+}
 
 export default function ProfilePhoto(): JSX.Element {
   const classes = useStyles();
 
-  return <Grid>This is the Profile Photo section.</Grid>
+  const [profilePhoto, setProfilePhoto] = useState('/assets/img/sample-profile.jpg');
 
+  return (
+    <Grid className={classes.root}>
+      <CssBaseline />
+      <Typography className={classes.settingsHeading}>Profile Photo</Typography>
+      <Avatar alt="sampleImage" src={profilePhoto} className={classes.large} />
+      <Typography className={classes.settingsSubheading}>
+        Be sure to use a photo that clearly shows your face.
+      </Typography>
+      <InputBase classes={{ input: classes.inputBase }} name="profilePhoto" inputComponent={InputButton} type="file" />
+      <Button>
+        <Icon component={DeleteForeverIcon} />
+        <Typography className={classes.deletePhotoText}>Delete Photo</Typography>
+      </Button>
+    </Grid>
+  );
 }
