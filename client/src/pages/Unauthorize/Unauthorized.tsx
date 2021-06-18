8ebc5e6243059055152986a6610bd49c6915af19
concoctionfactory <david.g.lee.nyc@gmail.com>
2021-06-17 23:09:00 -0400

    add margin

--- client/src/pages/Unauthorize/Unauthorized.tsx
+++ client/src/pages/Unauthorize/Unauthorized.tsx
@@ -4,11 +4,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Link from '@material-ui/core/Link';
 import { Link as RouterLink } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import { Box } from '@material-ui/core';
 
 export default function Unauthorized(): JSX.Element {
   return (
     <Grid container component="main" alignItems="center" direction="column">
       <CssBaseline />
+      <Box mt={5} />
       <Typography variant="h3" color="inherit" noWrap>
         Unauthorized
       </Typography>
