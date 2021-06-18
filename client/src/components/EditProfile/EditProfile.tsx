b43c137b4f1246e99143745fcd89f228661e0e6e
Wilson Fong <wilsonfong1002@outlook.com>
2021-06-17 23:18:28 -0400

    initial commit

--- client/src/components/EditProfile/EditProfile.tsx
+++ client/src/components/EditProfile/EditProfile.tsx
@@ -1,8 +1,89 @@
-import { Grid } from '@material-ui/core';
+import { useState } from 'react';
+import { Grid, Typography, TextField, Button, Switch } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import useStyles from './useStyles';
 
 export default function EditProfile(): JSX.Element {
   const classes = useStyles();
 
-  return <Grid>This is the Edit Profile section.</Grid>
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [selfDescription, setSelfDescription] = useState('');
+  const [phoneNumbers, setPhoneNumbers] = useState([]);
+
+  return (
+    <Grid className={classes.root}>
+      <CssBaseline />
+      <form>
+        <Typography className={classes.settingsHeading}>Edit Profile</Typography>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>I&apos;M AVAILABLE</Typography>
+          <Switch />
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>AVAILABILITY</Typography>
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>FIRST NAME</Typography>
+          <TextField
+            className={`${classes.formInput}`}
+            size="small"
+            variant="outlined"
+            placeholder="John"
+            type="string"
+          />
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>LAST NAME</Typography>
+          <TextField
+            className={`${classes.formInput}`}
+            size="small"
+            variant="outlined"
+            placeholder="Doe"
+            type="string"
+          />
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>EMAIL ADDRESS</Typography>
+          <TextField
+            className={`${classes.formInput}`}
+            size="small"
+            variant="outlined"
+            placeholder="john-doe@gmail.com"
+          />
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>PHONE NUMBER</Typography>
+          {phoneNumbers.length === 0 ? (
+            <Grid className={classes.phoneContainer}>
+              <Typography className={classes.phoneText}> No phone number entered </Typography>
+              <Button className={classes.phoneButton} variant="outlined" color="secondary">
+                Add a phone number
+              </Button>
+            </Grid>
+          ) : (
+            phoneNumbers
+          )}
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>WHERE YOU LIVE</Typography>
+          <TextField className={`${classes.formInput}`} size="small" variant="outlined" placeholder="Address" />
+        </Grid>
+        <Grid className={classes.formItem}>
+          <Typography className={classes.formLabel}>DESCRIBE YOURSELF</Typography>
+          <TextField
+            className={`${classes.formInput} ${classes.formTextField}`}
+            size="small"
+            variant="outlined"
+            placeholder="About you"
+            type="string"
+            multiline={true}
+            rows={4}
+          />
+        </Grid>
+      </form>
+    </Grid>
+  );
 }
--- client/src/components/EditProfile/useStyles.ts
+++ client/src/components/EditProfile/useStyles.ts
@@ -1,5 +1,47 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { CenterFocusStrong } from '@material-ui/icons';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  settingsHeading: {
+    fontSize: '1.4rem',
+    margin: '50px 0',
+    fontWeight: 800,
+    textAlign: 'center',
+  },
+  formItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: '20px 0px',
+    alignItems: 'center',
+  },
+  formLabel: {
+    fontWeight: 800,
+    width: '30%',
+    textAlign: 'right',
+  },
+  formInput: {
+    width: '70%',
+    marginLeft: '20px',
+  },
+  formTextField: {
+    multiline: true,
+    rows: 10,
+  },
+  phoneText: {
+    fontStyle: 'italic',
+    fontWeight: 600,
+    marginLeft: '20px',
+  },
+  phoneButton: {
+    fontWeight: 600,
+    marginLeft: '15px',
+  },
+  phoneContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+}));
 
 export default useStyles;
