f681ce430d8c543bc4984825c55dc1736ce047e6
concoctionfactory <david.g.lee.nyc@gmail.com>
2021-06-24 23:10:24 -0400

    missing comma

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const multer = require("multer")
-const { uploader, storage } = require("../cloudinary")
-const upload = multer({ storage })
+const multer = require("multer");
+const { uploader, storage } = require("../cloudinary");
+const upload = multer({ storage });
 const {
   newProfile,
   editProfile,
@@ -11,7 +11,7 @@ const {
   findSitters,
   uploadPhoto,
   changeMainPhoto,
-  deletePhoto
+  deletePhoto,
   findSittersByLocation,
 } = require("../controllers/profile");
 
@@ -19,9 +19,9 @@ router.route("/new-profile").post(protect, newProfile);
 router.route("/edit-profile/:id").patch(protect, editProfile);
 router.route("/get-profile/:id").get(protect, getProfile);
 router.route("/sitters").get(protect, findSitters);
-router.route("/upload-photo").post(upload.single("file"), uploadPhoto)
-router.route("/change-main-photo/:id").patch(protect, changeMainPhoto)
-router.route("/delete-photo").delete(protect, deletePhoto)
+router.route("/upload-photo").post(upload.single("file"), uploadPhoto);
+router.route("/change-main-photo/:id").patch(protect, changeMainPhoto);
+router.route("/delete-photo").delete(protect, deletePhoto);
 router.route("/location/:search").get(protect, findSittersByLocation);
 
 module.exports = router;
